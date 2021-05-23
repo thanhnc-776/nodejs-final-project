25a1d909fa6b55885c704b4d2db8e035a373e609
Nguyen Thanh <ncthanhla@gmail.com>
2021-05-23 16:32:21 +0700

    add nodemon

--- app.js
+++ app.js
@@ -38,7 +38,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log("Server run on PORT " + PORT));
-
 module.exports = app;
