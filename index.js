6036c9ab6be7cab2018e6223964abaa870d47143
Nguyen Thanh <ncthanhla@gmail.com>
2021-05-23 15:48:36 +0700

    add module

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.listen(3000, () => console.log("Server run on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server run on port 3000"));
 
 module.exports = app;
